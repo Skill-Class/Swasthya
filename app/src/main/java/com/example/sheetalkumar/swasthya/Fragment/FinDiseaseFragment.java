2b07de47ad344627a1022a98fdfa12a5219a2129
sheetalkumar <sheetal.mca17.du@gmail.com>
2019-01-26 00:19:09 +0530

    Listview added.

--- app/src/main/java/com/example/sheetalkumar/swasthya/Fragment/FinDiseaseFragment.java
+++ app/src/main/java/com/example/sheetalkumar/swasthya/Fragment/FinDiseaseFragment.java
@@ -8,7 +8,11 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.TextView;
 
+import com.airbnb.lottie.LottieAnimationView;
 import com.example.sheetalkumar.swasthya.R;
 public class FinDiseaseFragment extends Fragment {
 
@@ -17,6 +21,10 @@ public class FinDiseaseFragment extends Fragment {
         @Dev - Sheetal Kumar
         Date - 23 Jan 2019
      */
+
+    private LottieAnimationView lottieAnimationView;
+    private TextView textView;
+
     @Override
     public void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -28,6 +36,18 @@ public class FinDiseaseFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         View rootView = inflater.inflate(R.layout.fragment_fin_disease, container, false);
+
+        lottieAnimationView = rootView.findViewById(R.id.intro_lottie_animation_view);
+        //lottieAnimationView.playAnimation();
+        Animation animation = AnimationUtils.loadAnimation(getActivity(), R.anim.bottom_to_top);
+        textView = rootView.findViewById(R.id.textView11);
+        textView.startAnimation(animation);
         return rootView;
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+        lottieAnimationView.playAnimation();
     }
 }
