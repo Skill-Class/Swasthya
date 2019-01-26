65b08c01fdbb2119390d01f9775fd39a8f6e5d12
sheetalkumar <sheetal.mca17.du@gmail.com>
2019-01-26 22:45:04 +0530

    current location added

--- app/src/main/java/com/example/sheetalkumar/swasthya/Fragment/FinDiseaseFragment.java
+++ app/src/main/java/com/example/sheetalkumar/swasthya/Fragment/FinDiseaseFragment.java
@@ -1,19 +1,33 @@
 package com.example.sheetalkumar.swasthya.Fragment;
 
 import android.content.Context;
+import android.content.pm.PackageManager;
+import android.location.Address;
+import android.location.Geocoder;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
 import android.net.Uri;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.support.v4.app.ActivityCompat;
 import android.support.v4.app.Fragment;
+import android.support.v4.content.ContextCompat;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.view.animation.Animation;
 import android.view.animation.AnimationUtils;
+import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.airbnb.lottie.LottieAnimationView;
 import com.example.sheetalkumar.swasthya.R;
+
+import java.util.List;
+import java.util.Locale;
+
 public class FinDiseaseFragment extends Fragment {
 
 
@@ -23,7 +37,10 @@ public class FinDiseaseFragment extends Fragment {
      */
 
     private LottieAnimationView lottieAnimationView;
-    private TextView textView;
+    private TextView locationText;
+    private Button locationButton;
+    private LocationManager locationManager;
+    private LocationListener locationListener;
 
     @Override
     public void onCreate(@Nullable Bundle savedInstanceState) {
@@ -37,17 +54,82 @@ public class FinDiseaseFragment extends Fragment {
                              Bundle savedInstanceState) {
         View rootView = inflater.inflate(R.layout.fragment_fin_disease, container, false);
 
-        lottieAnimationView = rootView.findViewById(R.id.intro_lottie_animation_view);
+        // lottieAnimationView = rootView.findViewById(R.id.intro_lottie_animation_view);
         //lottieAnimationView.playAnimation();
-        Animation animation = AnimationUtils.loadAnimation(getActivity(), R.anim.bottom_to_top);
-        textView = rootView.findViewById(R.id.textView11);
-        textView.startAnimation(animation);
+        // Animation animation = AnimationUtils.loadAnimation(getActivity(), R.anim.bottom_to_top);
+        // textView = rootView.findViewById(R.id.textView11);
+        //textView.startAnimation(animation);
+
+        //fetching current location of user
+
+        locationText = rootView.findViewById(R.id.locationText);
+        locationButton = rootView.findViewById(R.id.getLocationBtn);
+
+
+        if (ContextCompat.checkSelfPermission(getActivity(),
+                android.Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED &&
+                ActivityCompat.checkSelfPermission(getActivity(),
+                        android.Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+
+            ActivityCompat.requestPermissions(getActivity(), new String[]{android.Manifest.permission.ACCESS_FINE_LOCATION, android.Manifest.permission.ACCESS_COARSE_LOCATION}, 101);
+
+        }
+
+        locationButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                getLocation();
+            }
+        });
+
+
         return rootView;
     }
 
-    @Override
+    private void getLocation() {
+        try {
+            locationManager = (LocationManager) getActivity().getSystemService(Context.LOCATION_SERVICE);
+            locationListener = new LocationListener() {
+                @Override
+                public void onLocationChanged(Location location) {
+                    locationText.setText("Latitude: " + location.getLatitude() + "\n Longitude: " + location.getLongitude());
+
+                    try {
+                        Geocoder geocoder = new Geocoder(getActivity(), Locale.getDefault());
+                        List<Address> addresses = geocoder.getFromLocation(location.getLatitude(), location.getLongitude(), 1);
+                        locationText.setText(locationText.getText() + "\n\nYour Current location is - " + "\n" + addresses.get(0).getAddressLine(0) + ", " +
+                                addresses.get(0).getAddressLine(1) + ", " + addresses.get(0).getAddressLine(2));
+                    } catch (Exception e) {
+
+                    }
+                }
+
+                @Override
+                public void onStatusChanged(String provider, int status, Bundle extras) {
+
+                }
+
+                @Override
+                public void onProviderEnabled(String provider) {
+                    Toast.makeText(getActivity(), "Please Enable GPS and Internet", Toast.LENGTH_SHORT).show();
+
+                }
+
+                @Override
+                public void onProviderDisabled(String provider) {
+
+                }
+            };
+
+            locationManager.requestLocationUpdates(LocationManager.NETWORK_PROVIDER, 5000, 5, locationListener);
+        } catch (SecurityException e) {
+            e.printStackTrace();
+        }
+    }
+
+   /* @Override
     public void onStart() {
         super.onStart();
         lottieAnimationView.playAnimation();
-    }
+    }*/
 }
